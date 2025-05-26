58312c22c2d582b772c376a556c061279923f75c
Caesarovich <cesar@caesarovich.xyz>
2025-05-26 18:43:51 +0200

    Remove unnecessary dependencies from bun.lock and package.json to streamline project setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,39 +1,50 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname , join} from 'node:path';
-import { Server } from 'socket.io';
+import { serve } from "bun";
+import homepage from "../frontend/index.html";
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
+type WebSocketData = {
+  createdAt: number;
+  uuid: string;
+}
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+type ServerWebSocket = Bun.ServerWebSocket<WebSocketData>;
 
+// Message WebSocket = on recoit des données peu importe 
+// Chat Message = un message de chat, on peut le publier sur un channel
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, '../frontend/index.html'));
+// https://bun.sh/docs/bundler/fullstack
+const server = serve({
+  development: true,
+
+  routes: {
+    "/": homepage,
+    "/_ws": (req, res) => {
+      // Meta-data
+      const data: WebSocketData = {
+        createdAt: Date.now(),
+        uuid: crypto.randomUUID(),
+      };
+
+      // Handle WebSocket connections
+      if (server.upgrade(req, {data})) {
+        return; // do not return a Response
+      }
+      return new Response("Upgrade failed", { status: 500 });
+    }
+  },
+
+  websocket: { 
+    message(ws: ServerWebSocket, message) {
+      console.log('WebSocket message received:', ws.data.uuid, message);
+      // Echo the message back to the client
+      server.publish('main', `Client ${ws} says: ${message}`);
+    }, // a message is received
+    open(ws: ServerWebSocket) {
+      console.log('WebSocket opened:', ws.data.uuid);
+      ws.subscribe('main'); // subscribe to a channel
+      ws.send('Welcome to the WebSocket server!'); // send a welcome message
+    }, // a socket is opened
+    close(ws: ServerWebSocket, code, message) {}, // a socket is closed
+  }
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+console.log(`Listening on ${server.url}`);
